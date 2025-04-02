e8da4e95ab6983c9d5e416039c112f6f71abf72f
Akarsh Rajput <akarshrajput@Akarshs-MacBook-Air.local>
2025-04-02 10:36:37 +0530

    Update UI

--- app/_components/main/Header.jsx
+++ app/_components/main/Header.jsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
 import React from "react";
+import Logo from "./Logo";
 
 const Header = () => {
   return (
     <div className="flex justify-center">
       <div className="max-w-[350px] border-violet-300 w-full bg-neutral-900 border-2 rounded-full p-2">
         <div className="flex items-center">
-          <p className="font-bold ml-3">CITEO</p>
+          <Logo />
           <div className="ml-auto text-sm flex items-center gap-3">
             {/* <p>Works</p>
             <p>Benefits</p> */}
--- app/_components/main/Logo.jsx
+++ app/_components/main/Logo.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Logo = () => {
-  return <div>Logo</div>;
+  return (
+    <div>
+      <img src="/logo.png" className="w-20 ml-4" />
+    </div>
+  );
 };
 
 export default Logo;
